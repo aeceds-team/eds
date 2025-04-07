feb4b2476a700033bc4bd8f6d0413bd444781f2d
aeceds-team <aeceds.team@gmail.com>
2025-04-07 14:40:21 +0530

    Initial customisation to card block.
    Initial customisation to card block.
    class name added for block, ul & li. created a separate div for Title and Description

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,18 +1,68 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  /* change to ul, li */
+
+  block.classList.add('services', 'equal', 'hover-anime', 'hardbg', 'border-grids');
   const ul = document.createElement('ul');
+  ul.className = 'row';
+
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
+    li.className = 'col-sm-4';
+
+    // move all row content into li
     while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+
+    const divs = [...li.children];
+    let imgDiv;
+    let textDiv;
+
+    // detect which div has image and which has text
+    divs.forEach((div) => {
+      if (div.querySelector('picture')) {
+        div.className = 'views-field views-field-nothing';
+        imgDiv = div;
+      } else {
+        textDiv = div;
+      }
     });
-    ul.append(li);
+
+    // process text div separately
+    if (textDiv) {
+      const strong = textDiv.querySelector('strong');
+      const ps = textDiv.querySelectorAll('p');
+
+      // remove original textDiv
+      textDiv.remove();
+
+      // create service-title
+      if (strong) {
+        const serviceTitle = document.createElement('div');
+        serviceTitle.className = 'service-title';
+        const a = document.createElement('a');
+        a.textContent = strong.textContent;
+        serviceTitle.appendChild(a);
+        li.appendChild(serviceTitle);
+      }
+
+      // create description
+      if (ps.length > 1) {
+        const desc = document.createElement('div');
+        desc.className = 'text';
+        desc.appendChild(ps[1].cloneNode(true));
+        li.appendChild(desc);
+      }
+    }
+
+    ul.appendChild(li);
   });
-  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+
+  // optimize all pictures
+  ul.querySelectorAll('picture > img').forEach((img) => {
+    const optimized = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
+    img.closest('picture').replaceWith(optimized);
+  });
+
   block.textContent = '';
   block.append(ul);
 }
